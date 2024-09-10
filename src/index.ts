7c83310e4e1cf98ad7ff6b83f0a3da63c539c3ff
murugesantechver <murugeshan.r@techversantinfotech.com>
2024-09-10 14:02:11 +0530

    haiiii

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript with Express and MongoDB!');
+  res.send('Hello, TypeScript with Express and MongoDB !!!!!!!!!!!!!!');
 });
 
 app.listen(port, () => {
